8659022932f32d48ae775cc76c41f95899c04297
Thomas LEDUC <info.leducthomas@gmail.com>
2018-09-20 11:47:35 +0200

    Init Apollo & react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Router, Route, browserHistory } from 'react-router'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { injectGlobal } from 'emotion'
+import Game from 'views/game/Game'
+import SelectTeam from 'views/select-team/SelectTeam'
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'
+})
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+injectGlobal`
+  @import 'https://fonts.googleapis.com/css?family=Open+Sans:300,400';
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Open Sans', sans-serif;
+  }
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <ApolloProvider client={client}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Welcome to React </h1>{' '}
-        </header>{' '}
-        <p className="App-intro">
-          To get started, edit <code> src / App.js </code> and save to reload.{' '}
-        </p>{' '}
-      </div>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Router history={browserHistory}>
+          <Route path="/" component={SelectTeam} />
+          <Route path="/game/:gameId" component={Game} />
+        </Router>
+      </ApolloProvider>
     )
   }
 }
---
+++ src/views/select-team/SelectTeam.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class SelectTeam extends React.Component {
+  render() {
+    return <div>Select your team</div>
+  }
+}
