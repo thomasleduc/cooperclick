c7b06ad39239e01409a7eb401f9607dd9bfe7721
Aranor28 <adrian.amigues@gmail.com>
2018-09-20 17:55:23 +0200

    Add dynamic hex layout size

--- src/components/hexagon/HexagonLayout.js
+++ src/components/hexagon/HexagonLayout.js
@@ -13,9 +13,19 @@ const HexagonLayoutContainer = styled('div')`
 `
 
 export default class HexagonLayout extends React.Component {
-  getRow(rowNumber, hexCount) {
+  static defaultProps = {
+    size: 1
+  }
+
+  layoutNeedMoreRows(rowNumber) {
+    console.log('rowNumber: ', rowNumber)
+    return Math.abs(rowNumber) < this.props.size
+  }
+
+  getRows(rowNumber, hexCount) {
     let hexes = []
     const rowOffsetLeft = (Math.abs(rowNumber) * (HEX_SIZE + MARGIN)) / 2
+
     for (let i = 0; i < hexCount; i++) {
       const hexOffsetLeft = i * HEX_SIZE + i * MARGIN
       hexes.push({
@@ -24,22 +34,43 @@ export default class HexagonLayout extends React.Component {
         id: rowNumber * 10 + (rowNumber >= 0 ? i : -i)
       })
     }
+
+    if (rowNumber === 0) {
+      return [
+        ...this.getRows(rowNumber + 1, hexCount - 1),
+        ...hexes,
+        ...this.getRows(rowNumber - 1, hexCount - 1)
+      ]
+    } else if (this.layoutNeedMoreRows(rowNumber)) {
+      return [
+        ...this.getRows(
+          rowNumber > 0 ? rowNumber + 1 : rowNumber - 1,
+          hexCount - 1
+        ),
+        ...hexes
+      ]
+    }
     return hexes
   }
 
-  getRecursiveRows() {}
+  // getRecursiveRows() {}
 
-  getHexagonPositions(size) {
-    return [...this.getRow(1, 2), ...this.getRow(0, 3), ...this.getRow(-1, 2)]
-  }
+  // getHexagonPositions(size) {
+  //   return [...this.getRow(1, 2), ...this.getRow(0, 3), ...this.getRow(-1, 2)]
+  // }
 
   render() {
-    const hexagons = this.getHexagonPositions()
+    const hexagons = this.getRows(0, 1 + this.props.size * 2)
     return (
       <HexagonLayoutContainer height={LAYOUT_HEIGHT}>
         <React.Fragment>
           {hexagons.map(hexagon => (
-            <HexagonTile x={hexagon.x} y={hexagon.y} id={hexagon.id} />
+            <HexagonTile
+              x={hexagon.x}
+              y={hexagon.y}
+              id={hexagon.id}
+              key={hexagon.id}
+            />
           ))}
         </React.Fragment>
       </HexagonLayoutContainer>
